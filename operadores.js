a4bad2c73d7516f056b42c8ea78e8d0de16194d5
Neilton Souza <neilton.sds1991@gmail.com>
2021-07-17 13:58:36 -0300

    primeiro commit

---
+++ operadores.js
@@ -0,0 +1,49 @@
+// opedadores aritmeticos
+// adição +
+var suntwoNumbers = 1+3
+console.log(suntwoNumbers)
+// subtração -
+var subTwoNumbers = 4-3
+console.log(subTwoNumbers)
+//multiplicação *
+var multTwoNumbers = 4*3
+console.log(multTwoNumbers)
+// divisão /
+var divtTwoNumbers = 4/2
+console.log(divtTwoNumbers)
+// resto da divisão %
+console.log(8%3)
+// incremento ++
+var num = 1;
+console.log(++num)
+//decremento --
+var num = 2
+console.log(--num)
+
+// operadores de atribuição
+// = atribuição
+// atribuição de soma
+ var x = 1
+ var y =  3
+ console.log(x+=y)
+ console.log(x-=y)
+ console.log(x*=y)
+ console.log(x/=y)
+ console.log(x%=y)
+
+ // operadores de comparação
+ // ==(compara valores)
+ // ===(compara valores e tipos)
+ // !=(diferente de)
+ //< ou <=(menor e menor igual)
+ //> ou >=(maior e maior igual)
+
+ // operadores lógicos
+ // e && (e)
+ // ou || (ou)
+ // ! (negação)
+
+
+
+
+ 
